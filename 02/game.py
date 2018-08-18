bfd4cd5c04906acec60f7405f2648dc151352ecc
Ken Doherty <kdoherty@carbonblack.com>
2018-08-18 17:54:52 -0400

    Compares user input to best word, validation needs work

--- 02/game.py
+++ 02/game.py
@@ -4,6 +4,7 @@
 
 import sys
 import random
+import itertools
 from data import DICTIONARY, LETTER_SCORES, POUCH
 
 NUM_LETTERS = 7
@@ -23,17 +24,23 @@ def draw_letters():
     return letters
 
 
-def get_possible_dict_words():
-    pass
+def get_possible_dict_words(letters):
+    allwords = []
+    for idx in range(1, 7):
+        for item in _get_permutations_draw(letters[:-idx]):
+            allwords.append(''.join(item).lower())
+    res = (set(allwords).intersection(DICTIONARY))
+    print(res)
+    return res
 
 
-def _get_permutations_draw():
-    pass
+def _get_permutations_draw(letters):
+    return itertools.permutations(letters)
 
 
 def _validation(user_word, letters):
-    for letter in user_word:
-        if letter not in letters:
+    for letter in user_word.lower():
+        if letter.lower() not in letters:
             return False
     return True
 
@@ -50,20 +57,40 @@ def max_word_value(words):
     return max(words, key=calc_word_value)
 
 
+def get_user_input():
+    return raw_input("Enter your word: ")
+
+
 def main():
     # Draw 7 letters from POUCH
     user_letters = draw_letters()
-    print("Letters drawn: {}".format(user_letters))
+    print("Letters drawn: {}".format(' '.join(user_letters)))
 
     # Get user's word
-    user_word = raw_input("Enter your word: ").upper()
+    user_word = raw_input("Enter your word: ")
+
+    while user_word not in DICTIONARY:
+        print('word is invalid')
+        user_word = get_user_input()
 
     # Verify user_word uses letters from draw
     if not _validation(user_word, user_letters):
         sys.exit("Your word uses letters you don't have")
-    print("Word chosen: {} (value: {})".format(user_word, calc_word_value(user_word)))
+    user_word_score = calc_word_value(user_word)
+    print("Word chosen: {} (value: {})".format(user_word, user_word_score))
 
-    # print(DICTIONARY)
+    # Get highest value word from letters drawn
+    possible_words = get_possible_dict_words(user_letters)
+    best_word = max_word_value(possible_words)
+    best_word_score = calc_word_value(best_word)
+
+    if user_word_score >= best_word_score:
+        print("CONGRATS! You have the highest word score!")
+        print("Best word: {} (value: {})".format(best_word, best_word_score))
+    elif best_word != user_word and user_word_score == best_word_score:
+        print("Another word with the same value: {} (value: {})".format(best_word.upper(), calc_word_value(best_word)))
+    else:
+        print("Best word is: {} (value: {})".format(best_word.upper(), best_word_score))
 
 
 if __name__ == "__main__":
