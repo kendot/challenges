cc2f7cb78cbbb3bd6f5c943e7da20206f4538c0a
Ken Doherty <kdoherty@carbonblack.com>
2018-08-19 14:08:07 -0400

    Not all various length words are being created

--- 02/game.py
+++ 02/game.py
@@ -24,13 +24,26 @@ def draw_letters():
     return letters
 
 
-def get_possible_dict_words(letters):
+def get_possible_dict_words(letters, user_word):
     allwords = []
-    for idx in range(1, 7):
-        for item in _get_permutations_draw(letters[:-idx]):
-            allwords.append(''.join(item).lower())
-    res = (set(allwords).intersection(DICTIONARY))
-    print(res)
+    print(letters)
+    for item in xrange(0, len(letters)+1):
+        els = [list(words) for words in itertools.combinations(letters, item)]
+        allwords.extend(els)
+
+    v = []
+    for group in allwords:
+        v.append(''.join(group).lower())
+
+    print(sorted(v))
+
+    if user_word in v:
+        print('!! FOUND USER WORD: {}'.format(user_word))
+    else:
+        print('!! USER WORD NOT FOUND: {}'.format(user_word))
+
+    res = (set(v).intersection(DICTIONARY))
+    print(sorted(res))
     return res
 
 
@@ -40,7 +53,7 @@ def _get_permutations_draw(letters):
 
 def _validation(user_word, letters):
     for letter in user_word.lower():
-        if letter.lower() not in letters:
+        if letter.upper() not in letters:
             return False
     return True
 
@@ -48,6 +61,7 @@ def _validation(user_word, letters):
 # re-use from challenge 01
 def calc_word_value(word):
     """Calc a given word value based on Scrabble LETTER_SCORES mapping"""
+    # print(word)
     return sum(LETTER_SCORES.get(char.upper(), 0) for char in word)
 
 
@@ -63,11 +77,12 @@ def get_user_input():
 
 def main():
     # Draw 7 letters from POUCH
+    print("-"*10)
     user_letters = draw_letters()
     print("Letters drawn: {}".format(' '.join(user_letters)))
 
     # Get user's word
-    user_word = raw_input("Enter your word: ")
+    user_word = raw_input("Enter your word: ").lower()
 
     while user_word not in DICTIONARY:
         print('word is invalid')
@@ -80,18 +95,19 @@ def main():
     print("Word chosen: {} (value: {})".format(user_word, user_word_score))
 
     # Get highest value word from letters drawn
-    possible_words = get_possible_dict_words(user_letters)
+    possible_words = get_possible_dict_words(user_letters, user_word)
     best_word = max_word_value(possible_words)
     best_word_score = calc_word_value(best_word)
 
     if user_word_score >= best_word_score:
         print("CONGRATS! You have the highest word score!")
         print("Best word: {} (value: {})".format(best_word, best_word_score))
-    elif best_word != user_word and user_word_score == best_word_score:
+    elif user_word_score == best_word_score and best_word != user_word:
         print("Another word with the same value: {} (value: {})".format(best_word.upper(), calc_word_value(best_word)))
     else:
         print("Best word is: {} (value: {})".format(best_word.upper(), best_word_score))
 
 
 if __name__ == "__main__":
-    main()
+    while True:
+        main()
